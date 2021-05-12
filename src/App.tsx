45c4d48bf5c42f8f5075cc80ca9f24567c031b88
Bram Borggreve <borggreve@gmail.com>
2021-05-12 12:00:01 -0500

    feat: connect to Test network + add Airdrop + error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,92 @@
-import { KinClient, KinProd, Wallet } from '@kin-sdk/client'
+import { createWallet, KinClient, KinTest, Wallet } from '@kin-sdk/client'
 import React, { useState } from 'react'
 
+const pfx = '?cluster=custom&customUrl=https%3A%2F%2Flocal.validator.agorainfra.dev'
+
+const txLink = (tx: string) => {
+  return `https://explorer.solana.com/tx/${tx}${pfx}`
+}
+const pkLink = (pk: string) => {
+  return `https://explorer.solana.com/address/${pk}${pfx}`
+}
+
 function App() {
-  const [client] = useState<KinClient>(() => new KinClient(KinProd))
+  const [client] = useState<KinClient>(() => new KinClient(KinTest))
   const [wallet, setWallet] = useState<Wallet>()
   const [created, setCreated] = useState<boolean>()
   const [step1Status, setStep1Status] = useState<string>()
   const [step2Status, setStep2Status] = useState<string>()
   const [step3Status, setStep3Status] = useState<string>()
+  const [step4Status, setStep4Status] = useState<string>()
+  const [txId, setTxId] = useState<string>()
 
-  const generateKeyPair = () => setWallet(KinClient.createWallet('create', {}))
+  const generateKeyPair = () => setWallet(createWallet('create', {}))
   const createAccount = async () => {
     setStep1Status('Creating Account Started')
-    const [result, error] = await client.createAccount(wallet?.secret!)
-    if (error) {
-      console.log('error', error)
-      setStep1Status(error)
-    } else {
-      setCreated(!!result)
-      setStep1Status('Creating Account Done ' + !!result)
+    try {
+      const [result, error] = await client.createAccount(wallet?.secret!)
+      if (error) {
+        console.log('error', error)
+        setStep1Status(error)
+      } else {
+        setCreated(!!result)
+        setStep1Status('Creating Account Done ' + JSON.stringify(result))
+      }
+    } catch (e) {
+      setStep1Status('Something went wrong!' + e)
     }
   }
   const resolveTokenAccount = async () => {
     setStep2Status('resolveTokenAccount')
-    const [result, error] = await client.resolveTokenAccounts(wallet?.publicKey!)
-    if (error) {
-      console.log('error', error)
-      setStep2Status(error)
-    } else {
-      setStep2Status(JSON.stringify(result))
+    try {
+      const [result, error] = await client.resolveTokenAccounts(wallet?.publicKey!)
+      if (error) {
+        console.log('error', error)
+        setStep2Status(error)
+      } else {
+        setStep2Status(JSON.stringify(result))
+      }
+    } catch (e) {
+      setStep2Status('Something went wrong!' + e)
     }
   }
+  const requestAirdrop = async () => {
+    setStep3Status('requestAirdrop')
+    try {
+      const [result, error] = await client.requestAirdrop(wallet?.publicKey!, '1000')
+      if (error) {
+        console.log('error', error)
+        setStep3Status(error)
+      } else {
+        await resolveTokenAccount()
+        setStep3Status(JSON.stringify(result))
+      }
+    } catch (e) {
+      setStep3Status('Something went wrong!' + e)
+    }
+  }
+
   const submitPayment = async () => {
-    setStep3Status('submitPayment')
-    const [result, error] = await client.submitPayment({
-      secret: wallet?.secret!,
-      tokenAccount: wallet?.publicKey!,
-      amount: '1',
-      destination: 'Don8L4DTVrUrRAcVTsFoCRqei5Mokde3CV3K9Ut4nAGZ',
-      memo: 'Test Payment',
-    })
-    if (error) {
-      console.log('error', error)
-      setStep3Status(error)
-    } else {
-      setStep3Status('Creating Account Done ' + !!result)
+    setStep4Status('submitPayment')
+    setTxId('')
+    try {
+      const [result, error] = await client.submitPayment({
+        secret: wallet?.secret!,
+        tokenAccount: wallet?.publicKey!,
+        amount: '10',
+        destination: 'Don8L4DTVrUrRAcVTsFoCRqei5Mokde3CV3K9Ut4nAGZ',
+        memo: 'Test Payment',
+      })
+      if (error) {
+        console.log('error', error)
+        setStep4Status(error)
+      } else {
+        await resolveTokenAccount()
+        setStep4Status('Submit Payment Done: ' + result)
+        setTxId(result)
+      }
+    } catch (e) {
+      setStep4Status('Something went wrong!' + e)
     }
   }
   return (
@@ -56,7 +98,9 @@ function App() {
       ) : (
         <div>
           <div>
-            <code>publicKey: {wallet.publicKey}</code>
+            <a href={pkLink(wallet.publicKey!)} target="_blank" rel="noreferrer">
+              <code>publicKey: {wallet.publicKey}</code>
+            </a>
           </div>
           <div>{created ? `Account Created ${created}` : <button onClick={createAccount}>Create Account</button>}</div>
           <pre>{step1Status}</pre>
@@ -67,8 +111,19 @@ function App() {
                 <pre>{step2Status}</pre>
               </div>
               <div>
-                <button onClick={submitPayment}>Submit Payment</button>
+                <button onClick={requestAirdrop}>Request Airdrop</button>
                 <pre>{step3Status}</pre>
+              </div>
+              <div>
+                <button onClick={submitPayment}>Submit Payment</button>
+                <pre>{step4Status}</pre>
+                {txId ? (
+                  <a href={txLink(txId)} target="_blank" rel="noreferrer">
+                    <code>{txId}</code>
+                  </a>
+                ) : (
+                  ''
+                )}
               </div>
             </div>
           ) : null}
